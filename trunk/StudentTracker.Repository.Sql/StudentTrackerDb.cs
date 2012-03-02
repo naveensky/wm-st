a5088b1a5af4aab43eb0f77bb805155cc9388037
Raman Mittal <rmittal@greenapplesolutions.com>
2012-03-02 17:05:12 +0530

    add the dbset for course,time,timeslots and users

--- trunk/StudentTracker.Repository.Sql/StudentTrackerDb.cs
+++ trunk/StudentTracker.Repository.Sql/StudentTrackerDb.cs
@@ -15,5 +15,10 @@ namespace StudentTracker.Repository.Sql {
         public IDbSet<Student> Students { get; set; }
         public IDbSet<Teacher> Teachers { get; set; }
         public IDbSet<Appointment> Appointments { get; set; }
+        public IDbSet<Course> Courses { get; set; }
+        public IDbSet<StudyCenter> StudyCenters { get; set; }
+        public IDbSet<Time> Times { get; set; }
+        public IDbSet<TimeSlot> TimeSlots { get; set; }
+        public IDbSet<User> Users { get; set; } 
     }
 }
